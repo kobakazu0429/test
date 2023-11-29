3c0bc901c0de242b94c93c0f588e7dbd13b635c6
kobakazu0429 <kobakazu10@gmail.com>
2023-11-30 01:54:00 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import path from "path";
 import { defineConfig } from "vite";
 
-module.exports = defineConfig({
+export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(path.join(__dirname, "src", "index.ts")),
